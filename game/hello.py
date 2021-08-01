fa45505aa4d9074c208cbb45aa8d54fa05bb0ddd
Kenny2github <kenny2minecraft@gmail.com>
2021-07-31 21:49:26 -0700

    Add example hello command

---
+++ game/hello.py
@@ -0,0 +1,14 @@
+from typing import List
+import argparse
+
+choices = ['hi', 'hello', 'hey']
+
+parser = argparse.ArgumentParser(description='Hello World!')
+parser.add_argument('word', nargs='?', choices=choices,
+                    help='The greeting word')
+
+completion = "-W '%s'" % ' '.join(choices)
+
+def main(args: List[str]):
+    cmdargs = parser.parse_args(args[1:])
+    print('Hello World!', (cmdargs.word or '').title())
