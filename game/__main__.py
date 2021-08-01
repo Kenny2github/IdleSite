3a831961729e6592284268ffbdf75e0506d238bc
Kenny2github <kenny2minecraft@gmail.com>
2021-07-31 23:11:27 -0700

    Add --save-slot option and track current slot
    If no current, none specified, and:
    * 0 available: create one named 1
    * 1 available: choose it
    * more available: ask to choose

--- game/__main__.py
+++ game/__main__.py
@@ -17,6 +17,39 @@ if '--completion' in sys.argv:
 else:
     completion = False
 
+if not completion: # all this machinery needs to be ignored for --completion
+    from game.i18n import i18n
+    open('saves/.current', 'a').close() # create empty if not exists
+    try:
+        idx = sys.argv.index('--save-slot')
+    except ValueError: # not specified at command line
+        with open('saves/.current', 'r') as current:
+            slot = current.read().strip()
+        if not slot: # none currently selected
+            slots = [name for name in os.listdir('saves')
+                    if name not in {'README.md', '.current', '.lang'}]
+            slot_count = len(slots)
+            if slot_count == 0:
+                slot = '1'
+                print(i18n('no-save-slots', slot))
+            elif slot_count == 1:
+                slot = slots[0]
+                print(i18n('one-save-slot', slot))
+            else:
+                print(i18n('many-save-slots'))
+                print('\n'.join(slots))
+                while slot not in slots:
+                    slot = input(i18n('save-slot-prompt'))
+            with open('saves/.current', 'w') as current:
+                current.write(slot) # save current slot
+    else: # specified at command line
+        del sys.argv[idx] # pop --save-slot argument
+        try:
+            slot = sys.argv[idx] # argument value shifted left
+            del sys.argv[idx] # pop argument value after reading
+        except IndexError:
+            sys.exit('--save-slot option requires an argument')
+
 try:
     game = importlib.import_module('game.' + command)
 except ImportError:
