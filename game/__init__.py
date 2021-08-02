372e97b93b592204bcd96da9bb15b5c1c87160e3
Kenny2github <kenny2minecraft@gmail.com>
2021-08-02 14:03:07 -0700

    Split current slot logic out of get_slot

--- game/__init__.py
+++ game/__init__.py
@@ -12,22 +12,7 @@ def get_slot(argv: list[str]) -> str:
         with open('saves/.current', 'r') as current:
             slot = current.read().strip()
         if not slot: # none currently selected
-            slots = [name for name in os.listdir('saves')
-                    if name not in {'README.md', '.current', '.lang'}]
-            slot_count = len(slots)
-            if slot_count == 0:
-                slot = '1'
-                pi18n('no-save-slots', slot)
-            elif slot_count == 1:
-                slot = slots[0]
-                pi18n('one-save-slot', slot)
-            else:
-                pi18n('many-save-slots')
-                print('\n'.join(slots))
-                while slot not in slots:
-                    slot = input(i18n('save-slot-prompt'))
-            with open('saves/.current', 'w') as current:
-                current.write(slot) # save current slot
+            slot = get_current_slot()
     else: # specified at command line
         del argv[idx] # pop --save-slot argument
         try:
@@ -35,6 +20,26 @@ def get_slot(argv: list[str]) -> str:
             del argv[idx] # pop argument value after reading
         except IndexError:
             sys.exit('--save-slot option requires an argument')
+    return slot
+
+
+def get_current_slot() -> str:
+    slots = [name for name in os.listdir('saves')
+             if name not in {'README.md', '.current', '.lang'}]
+    slot_count = len(slots)
+    if slot_count == 0:
+        slot = '1'
+        pi18n('no-save-slots', slot)
+    elif slot_count == 1:
+        slot = slots[0]
+        pi18n('one-save-slot', slot)
+    else:
+        pi18n('many-save-slots')
+        print('\n'.join(slots))
+        while slot not in slots:
+            slot = input(i18n('save-slot-prompt'))
+    with open('saves/.current', 'w') as current:
+        current.write(slot)  # save current slot
     return slot
 
 def load_slot(slot: str) -> SaveSlot:
