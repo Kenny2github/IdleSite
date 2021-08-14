3d92eadc4c2222f805279cae33cf3f8f402c89f9
Kenny2github <kenny2minecraft@gmail.com>
2021-08-13 23:55:34 -0400

    Implement cancelling transactions

---
+++ game/cancel.py
@@ -0,0 +1,23 @@
+import argparse
+from game.slot import SaveSlot
+from .i18n import i18n
+
+TYPES = {
+    'transaction': 'transactions_pending',
+}
+
+parser = argparse.ArgumentParser(description=i18n('cancel-desc'))
+parser.add_argument('type', choices=list(TYPES.keys()),
+                    help=i18n('cancel-type-opt'))
+parser.add_argument('indexes', nargs='+', type=int, metavar='index',
+                    help=i18n('cancel-index-opt'))
+
+completion = f"-o nosort -W '-h --help {' '.join(TYPES)}'"
+
+def main(args: list[str], slot: SaveSlot):
+    cmdargs = parser.parse_args(args[1:])
+    attr = TYPES[cmdargs.type]
+    # slot.<attr> = [<all items not matching indexes>]
+    setattr(slot, attr, [
+        v for i, v in enumerate(getattr(slot, attr), start=1)
+        if i not in cmdargs.indexes])
